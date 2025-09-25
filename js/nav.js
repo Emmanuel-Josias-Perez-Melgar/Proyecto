f73ec2a9bb194c391eb1a987c81450fb0cba1bb5
Emmanuel-Perez <20142697s@gmail.com>
2025-09-25 10:53:34 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,7 @@
 const menu = document.getElementById('menu');
 const sidebar = document.getElementById('sidebar');
-const main = document.getElementById('main')
 
-menu.addEventListener('click', ()=>{
-    sidebar.classList.toggle('menu-toggle');
-    menu.classList.toggle('menu-toggle');
-    main.classList.toggle('menu-toggle');
+menu.addEventListener('click', () => {
+  sidebar.classList.toggle('menu-toggle');
+  menu.classList.toggle('menu-toggle');
 });
